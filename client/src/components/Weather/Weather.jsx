6b7dd729f5ec33a51f0397ba218b479641b1dce6
Serena Entezary <serena.entezary@gmail.com>
2021-04-05 15:24:58 -0400

    Update weather bar and LocationPins so that they have the correct clickable items and display correct data

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DailyWeather from '../DailyWeather/DailyWeather';
 
 const Weather = ({ data, selectedCity }) => {
-  const [ isClosed, setIsClosed ] = useState(false);
-
-  const handleClose = () => {
-    setIsClosed(true);
-  }
-
-  const handleOpen = () => {
-    setIsClosed(false);
-  }
-
   return (
-    <>
-      <div aria-label="Click here to reopen weather results" className={`reopen is-displayed-${isClosed}`} onClick={handleOpen}>
-        <a className="display-results" href="#">Click here to display weather results</a>
+    <div className="weather-bar">
+      <div className="city-header">
+        <h2>
+          {selectedCity.city}
+        </h2>
+        <DailyWeather data={data} />
       </div>
-      <div className={`weather-bar is-closed-${isClosed}`}>
-        <button aria-label="Click here to close the weather information" className="close"
-          onClick={handleClose}>X</button>
-        <div className="city-header">
-          <h2>
-            {selectedCity.city}
-          </h2>
-          <DailyWeather data={data} />
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
 
