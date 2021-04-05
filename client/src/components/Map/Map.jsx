2fc8f22b9304d0c521411d89bd8549fbcfee4b9d
Serena Entezary <serena.entezary@gmail.com>
2021-04-05 15:25:39 -0400

    Update the Map component and update constants to have the correct data

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,19 +1,30 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import GoogleMapReact from 'google-map-react'
 import LocationPin from '../LocationPin/LocationPin'
-import { cityLocations } from '../../constants';
 import Weather from '../Weather/Weather';
+import { cityLocations } from '../../constants'; 
 import { isNullOrUndefined } from '../../utils/helpers';
 
-const Map = ({ location = cityLocations[2].data.google, zoomLevel }) => {
+const Map = ({ location = cityLocations[2], zoomLevel }) => {
   const [ chosenCity, setChosenCity ] = useState(cityLocations[2]);
   const [ weatherLocationData, setWeatherLocationData ] = useState('');
   const [ data, setData ] = useState([]);
-  
+  const [ isButtonSelected, setIsButtonSelected ] = useState({ city: chosenCity.city, isSelected: false});
+  const [ isClosed, setIsClosed ] = useState(true);
+  const [ apiKey, setApiKey ] = useState('');
+
+  const handleClose = () => {
+    setIsClosed(true);
+  }
+
+  const handleOpen = () => {
+    setIsClosed(false);
+  }
+
   const getData = async (selectedCity = chosenCity) => {
-    const lat = selectedCity.data.weather.coord.lat;
-    const lon = selectedCity.data.weather.coord.lon;
+    const lat = selectedCity.data.lat;
+    const lon = selectedCity.data.lng;
 
     const weatherLocationDataUrl = await axios.get(`https://api.weather.gov/points/${lat},${lon}`)
       .then(res => {
@@ -35,44 +46,87 @@ const Map = ({ location = cityLocations[2].data.google, zoomLevel }) => {
       return getWeather;
   }
 
+  const handleCityClick = (target, citySelector) => {
+    if (target === citySelector.city && citySelector !== chosenCity) {
+      setChosenCity(citySelector);
+      setIsButtonSelected({ city: citySelector.city, isSelected: true });
+      getData(citySelector);
+    }
+  }
+
+  const getURLKey = async () => {
+    await axios.get('http://localhost:3001/api')
+    .then(response => {
+      return setApiKey(response.data.key);
+    })
+    .catch(error => console.error(error));
+  }
+
   useEffect(() => {
-    getData(chosenCity);
-  }, [])
+    getURLKey();
+    setIsButtonSelected({ city: chosenCity.city, isSelected: true });
+    getData();
+  }, []);
 
   return (
     <>
-      <div className="map">
-        <div className="google-map">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyCLqxS2Hlhf0Zrz70tMqEs-dezJcYZ_V04' }}
-            defaultCenter={location}
-            defaultZoom={zoomLevel}
+      { apiKey !== '' ? <div className="map">
+          <div className="google-map">
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: apiKey }}
+              defaultCenter={location.data}
+              defaultZoom={zoomLevel}
+            >
+              {cityLocations.map((pinLocation) => {
+                const googleData = pinLocation.data;
+                
+                return <LocationPin
+                  key={pinLocation.city}
+                  lat={googleData.lat}
+                  lng={googleData.lng}
+                  onClick={() => handleCityClick(pinLocation.city, pinLocation)}
+                  text={pinLocation.city}
+                />
+              })}
+            </GoogleMapReact>
+          </div>
+        </div> : '' }
+      <button
+          aria-label="Click here to see weather results" 
+          className={`reopen is-displayed-${isClosed}`}
+          onClick={handleOpen}
+        >
+          <p className="display-results">Click here to display weather results</p>
+        </button>
+      <div className={`bottom-section is-closed-${isClosed}`}>
+        <button
+          aria-label="Click here to close the weather information"
+          className="close"
+          onClick={handleClose}
           >
-            {cityLocations.map((location) => {
-              const googleData = location.data.google;
-
-              const handleCityClick = target => {
-                if (target === location.city) {
-                  console.log(location);
-                  setChosenCity(location);
-                  getData(location);
-                }
-              }            
-              
-              return <LocationPin
-                key={location.city}
-                lat={googleData.lat}
-                lng={googleData.lng}
-                onClick={() => handleCityClick(location.city)}
-                text={location.city}
-              />
-            })}
-          </GoogleMapReact>
+            X
+        </button>
+        <div className="button-selectors">
+          {cityLocations.map((buttonLocation, i) => {
+            const addSelectedClass = isButtonSelected.isSelected && isButtonSelected.city === buttonLocation.city;
+            const buttonClass = addSelectedClass ? ' button-selected' : '';
+            return (
+              <button 
+                className={`city-button${buttonClass}`}
+                key={`button-${i}`}
+                onClick={() => handleCityClick(buttonLocation.city, buttonLocation)}
+                type="button"
+              >
+                {buttonLocation.city}
+              </button>
+            )
+          })}
         </div>
+        
+        <Weather
+          data={data}
+          selectedCity={chosenCity} />
       </div>
-      <Weather
-        data={data}
-        selectedCity={chosenCity} />
     </>
   )  
 }
